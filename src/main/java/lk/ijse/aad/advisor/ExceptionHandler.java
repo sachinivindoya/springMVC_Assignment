1d53667182d994e11d9dbc57101deb99ef974cf5
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2023-09-22 11:41:34 +0530

    exception handler

---
+++ src/main/java/lk/ijse/aad/advisor/ExceptionHandler.java
@@ -0,0 +1,19 @@
+package lk.ijse.aad.advisor;
+
+import lk.ijse.aad.util.ResponseUtil;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@CrossOrigin
+public class ExceptionHandler {
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @org.springframework.web.bind.annotation.ExceptionHandler({Exception.class})
+
+    public ResponseUtil exceptionHandler(Exception e){
+        return new ResponseUtil(500,e.getMessage(),null);
+    }
+}
