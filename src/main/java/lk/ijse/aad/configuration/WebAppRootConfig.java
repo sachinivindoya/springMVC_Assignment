0a90ace017a1d41ab34f9d24e256a7f7e1d4dfc7
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2023-09-22 11:06:39 +0530

    WebAppRootConfig

--- src/main/java/lk/ijse/aad/configuration/WebAppRootConfig.java
+++ src/main/java/lk/ijse/aad/configuration/WebAppRootConfig.java
@@ -1,4 +1,18 @@
 package lk.ijse.aad.configuration;
 
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Controller
+@EnableWebMvc
+@ComponentScan
+@Import(HibernateConfig.class)
 public class WebAppRootConfig {
+
+    public WebAppRootConfig(){
+        System.out.println("WebAppRootConfig : start");
+    }
 }
