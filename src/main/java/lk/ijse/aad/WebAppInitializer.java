90575e0e05b256f52bcfd256726c18551bbca0ea
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2023-09-22 10:57:45 +0530

    webAppInitializer

--- src/main/java/lk/ijse/aad/WebAppInitializer.java
+++ src/main/java/lk/ijse/aad/WebAppInitializer.java
@@ -2,6 +2,20 @@ package lk.ijse.aad;
 
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-public class WebAppInitializer {
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
 
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebAppRootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebAppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
 }
